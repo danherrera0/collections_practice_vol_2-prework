05a7d6aaeba83d6fd6dd20945fc439c63b64a7f3
Dania Herrera <danherrera0@gmail.com>
2018-12-02 19:04:50 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,47 +1,64 @@
 # your code goes here
-def begins_with_r(tools)
-  tools.each do |item|
-    if item[0]== "r"
+require 'pry'
+def begins_with_r(array)
+  array.each do |x|
+    # binding.pry
+    if x[0]== "r"
       return true
-    else return false
+    else
+      return false
+end
+end
 end
 
 
 def contain_a(array)
-  array.each do |element|
-    if element.include("a")
-      puts element
-    end
-
-
-def first_wa(array)
-  array.each do |element|
-    if element[0]== "w" && element[1]=="a"
-      return element
-      break
-    end
-
-def remove_non_strings(array)
-  array.each do |element|
-    deleteif element.is_a?(string) == false
+a_array = []
+array.each do |x|
+if x.include?("a") == true
+a_array << x
+end
+end
+binding.pry
+a_array
 end
 
-def count_elements(array)
-  array.each do |element|
-    array.count(element)
-  end
-
-def merge_data(v1,v2)
-  v1[0].values.map.with_index {|v, i| v2[i].merge(v)}
-end
-
-def find_cool(array)
-  array.select{|key, hash|
-    if hash == "cool"
-    puts hash
-end
-
-def organize_schools(array)
-  array.each do |element|
-    
-end
+# def first_wa(array)
+#   array.each do |element|
+#     if element[0]== "w" && element[1]=="a"
+#       return element
+#       break
+#     end
+#   end
+# end
+#
+#
+# def remove_non_strings(array)
+#   array.each do |element|
+#     deleteif element.is_a?(string) == false
+# end
+# end
+#
+#
+# def count_elements(array)
+#   array.each do |element|
+#     array.count(element)
+#   end
+# end
+#
+# def merge_data(v1,v2)
+#   v1[0].values.map.with_index {|v, i| v2[i].merge(v)}
+# end
+#
+# def find_cool(array)
+#   array.select{|key, hash|
+#     if hash == "cool"
+#     puts hash
+# end
+# end
+# end
+#
+# def organize_schools(array)
+#   array.each do |element|
+# end
+# end
